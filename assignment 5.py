3a0930acfd26ae2b01d14894523b3e19d6265c8e
applelooking <gys_921020@163.com>
2015-11-09 21:05:06 -0800

    fix the 5.2 code, just forget to upload this morning

--- assignment 5.py
+++ assignment 5.py
@@ -5,7 +5,7 @@ data_Y = []
 for l in open('hw5_X.txt'):
 	data_X.append([int(j) for j in l.split()])
 for l in open('hw5_Y.txt'):
-	data_Y.append(l)
+	data_Y.append(int(l))
 prob = []
 # initial value
 for i in range(10):
@@ -17,7 +17,7 @@ def denom(X):
 		total *= ((1.0 - prob[i])**X[i])
 	return 1.0 - total
 
-def p(X, Y, i):
+def pr(X, Y, i):
 	return Y * 1.0 * X[i] * prob[i] / denom(X)
 
 def p(X, Y):
@@ -33,8 +33,21 @@ def ll(X_list, Y_list):
 		total += math.log(p(X_list[i], Y_list[i]))
 	return total / len(Y_list)
 
+def update():
+	global prob
+	tmp = list(prob)
+	for i in range(len(tmp)):
+		total = 0.0
+		counter = 0
+		for j in range(len(data_Y)):
+			total += pr(data_X[j], data_Y[j], i)
+			if data_X[j][i] == 1:
+				counter += 1
+		tmp[i] = total / counter
+	prob = tmp
 def main():
-	print ll(data_X, data_Y)
+	update()
+	print ("% .4f" % ll(data_X, data_Y))
 
 if __name__ == '__main__':
 	main()
